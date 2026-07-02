c6a2bf4f4950f1ef8d99624742013febec998610
GianniCarvacho-Prsnl <giannicarvacho@gmail.com>
2026-07-01 22:25:45 -0400

    Use tagged release in Homebrew formula

--- Formula/count-focus.rb
+++ Formula/count-focus.rb
@@ -1,8 +1,9 @@
 class CountFocus < Formula
   desc "Focus timer for the terminal"
   homepage "https://github.com/gianni-labs/count-focus"
-  url "https://github.com/gianni-labs/count-focus.git", branch: "main"
-  version "0.1.0-dev"
+  url "https://codeload.github.com/gianni-labs/count-focus/tar.gz/refs/tags/v0.1.0"
+  sha256 "ea88f7ff34cf690ccab6695c724c7f27a5c4b4869a6d21d0cbed8c2a331c23ee"
+  license "MIT"
 
   depends_on "go" => :build
 
